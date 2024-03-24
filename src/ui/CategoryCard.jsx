ec92aab57e203bfd927a6135aa9c098d8d2c0d25
3nonch <enonch@hotmail.com>
2024-03-24 15:44:19 +0300

    fetching categories to display on homepage

--- src/ui/CategoryCard.jsx
+++ src/ui/CategoryCard.jsx
@@ -1,16 +1,22 @@
-export default function CategoryCard() {
+import { Link } from "react-router-dom";
+import { scrollTop, trToEn } from "../features/helperutils";
+
+/* eslint-disable react/prop-types */
+const CategoryCard = ({ category }) => {
   return (
     <div className="relative flex h-[100%]">
-      <img
-        className="w-[100%] h-[100%] object-cover"
-        src="/category-dummy.png"
-      />
-      <a
-        href="#"
+      <img className="w-[100%] rounded-md object-cover" src={category?.img} />
+      <Link
+        onClick={scrollTop}
+        to={`/shop/${trToEn(category.gender)}/${trToEn(
+          category.title.toLowerCase()
+        )}`}
         className="absolute bottom-10 left-10 text-[1.6rem] bg-white px-[5rem] py-[1rem]"
       >
-        MEN
-      </a>
+        {category?.title}
+      </Link>
     </div>
   );
-}
+};
+
+export default CategoryCard;
--- src/ui/FeaturedCategory.jsx
+++ src/ui/FeaturedCategory.jsx
@@ -1,30 +1,45 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useSelector } from "react-redux";
 import CategoryCard from "./CategoryCard";
+import { selectAllCategories } from "../app/globalSlice";
+import MiniLoader from "./MiniLoader";
 
 const FeaturedCategory = () => {
+  const featuredCategories = useSelector(selectAllCategories);
   return (
-    <div className="my-[3rem] max-md:px-[1rem] container mx-auto">
-      <div className="flex flex-col items-center justify-center mb-[3rem] ">
+    <>
+      <div className="flex flex-col items-center justify-center pt-[2rem] mb-[3rem] ">
         <h2 className="uppercase">Editor's Pick</h2>
         <p>Problems trying to resolve the conflict between</p>
       </div>
-      <div className="flex flex-wrap justify-between gap-[3rem] max-sm:gap-[1rem]">
-        <div className="max-sm:basis-[40%] max-md:basis-[35%] basis-2/5 flex-1">
-          <CategoryCard />
+      {!featuredCategories.length ? (
+        <div
+          className="w-[100%] h-[30rem] flex justify-center items-center
+        ]"
+        >
+          <MiniLoader />
         </div>
-        <div className="max-sm:basis-[40%] max-md:basis-[60%] basis-1/5 flex-1 bg-black">
-          <CategoryCard />
-        </div>
-        <div className="max-md:flex-row basis-1/5 flex-1 flex flex-col gap-[3rem] max-sm:gap-[1rem]">
-          <div className="bg-black max-sm:flex-1 max-md:basis-[60%]">
-            <CategoryCard />
+      ) : (
+        <div className="my-[3rem] max-md:px-[1rem] container mx-auto">
+          <div className="flex flex-wrap justify-between gap-[3rem] max-sm:gap-[1rem]">
+            <div className="max-sm:basis-[40%] max-md:basis-[35%] basis-2/5 flex-1 max-h-[60rem]">
+              <CategoryCard category={featuredCategories[10]} />
+            </div>
+            <div className="max-sm:basis-[40%] max-md:basis-[60%] basis-1/5 flex-1 max-h-[60rem]">
+              <CategoryCard category={featuredCategories[8]} />
+            </div>
+            <div className="max-md:flex-row basis-1/5 flex-1 flex flex-col gap-[3rem] max-sm:gap-[1rem] max-h-[60rem]">
+              <div className="bg-black max-sm:flex-1 max-md:basis-[60%]  md:max-h-[30rem]">
+                <CategoryCard category={featuredCategories[4]} />
+              </div>
+              <div className="bg-black max-sm:flex-1 max-md:basis-[35%]  md:max-h-[27rem]">
+                <CategoryCard category={featuredCategories[2]} />
+              </div>
+            </div>
           </div>
-          <div className="bg-black max-sm:flex-1 max-md:basis-[35%]">
-            <CategoryCard />
-          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
