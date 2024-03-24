a4910391994e8da9328f5eb12cef5ac03c63a0c8
3nonch <enonch@hotmail.com>
2024-03-24 15:44:55 +0300

    A miniloader component for sections

---
+++ src/ui/MiniLoader.jsx
@@ -0,0 +1,18 @@
+const MiniLoader = () => {
+  return (
+    <div className="flex w-[100%] h-[40rem] justify-center items-center">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default MiniLoader;
