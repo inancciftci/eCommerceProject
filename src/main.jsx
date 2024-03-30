ed2f9b89ec786fcebe0dfc51c9909cf1799e1133
3nonch <enonch@hotmail.com>
2024-03-30 09:09:49 +0300

    ToastContainer added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 import { fecthCategories, fetchRoles } from "./app/globalSlice.js";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 store.dispatch(fetchRoles());
 store.dispatch(fecthCategories());
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
