da3600809bf1569123f2b248dcd40052a5214cc9
3nonch <enonch@hotmail.com>
2024-05-25 12:16:59 +0300

    fixed it for mobile

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -11,7 +11,7 @@ const CartItem = ({ product }) => {
   const dispatch = useDispatch();
   const categories = useSelector(selectAllCategories);
   const category = categories.find(
-    (item) => item.id === product.product.category_id,
+    (item) => item.id === product.product.category_id
   );
   const quantityHandler = (e) => {
     if (e.target.name === "+") {
@@ -36,7 +36,7 @@ const CartItem = ({ product }) => {
           className="object-cover w-[100%] max-h-[10rem]"
         />
       </div>
-      <div className="w-[100%] flex justify-between">
+      <div className="w-[100%] flex justify-between max-md:flex-col">
         <div className=" flex flex-col gap-[1rem] ">
           <p>
             <span className="font-[500] text-[1.4rem]">{category?.title}</span>
@@ -44,12 +44,47 @@ const CartItem = ({ product }) => {
               {product?.product.name}
             </span>
           </p>
-          <p className="text-slate-400">
+          <p className="text-slate-400 max-md:text-[1.2rem]">
             <i className="fa-solid fa-truck-fast mr-[0.5rem]"></i>
             Estimated Shipping Time: in 2 days
           </p>
         </div>
-        <div className="flex">
+        <div className="hidden max-md:flex justify-between items-center pt-[1rem]">
+          <div className="flex">
+            <button
+              onClick={quantityHandler}
+              name="-"
+              className="counter-button"
+            >
+              -
+            </button>
+            <input
+              className="counter-content"
+              type="text"
+              pattern="[0-9]*"
+              value={product.quantity}
+            />
+            <button
+              onClick={quantityHandler}
+              name="+"
+              className="counter-button"
+            >
+              +
+            </button>
+          </div>
+          <div className="text-right flex flex-col gap-[0.3rem]">
+            <button
+              onClick={() => handleDeleteProduct(product)}
+              className="text-[1rem] font-[300] text-slate-400"
+            >
+              <i className="fa-solid fa-trash-can mr-[0.3rem]"></i>Remove Item
+            </button>
+            <h5 className="text-[#6074c2]">
+              {(product.product.price * product.quantity).toFixed(2)} TL
+            </h5>
+          </div>
+        </div>
+        <div className="flex max-md:hidden">
           <button onClick={quantityHandler} name="-" className="counter-button">
             -
           </button>
@@ -63,7 +98,7 @@ const CartItem = ({ product }) => {
             +
           </button>
         </div>
-        <div className="text-right flex flex-col gap-[0.3rem]">
+        <div className="text-right flex flex-col gap-[0.3rem] max-md:hidden">
           <button
             onClick={() => handleDeleteProduct(product)}
             className="text-[1rem] font-[300] text-slate-400"
