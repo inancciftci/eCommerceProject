c66bd950e58a0718b00eeede714088aba53afaf0
3nonch <enonch@hotmail.com>
2024-05-28 16:44:07 +0300

    addressSlice added to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import globalReducer from "../app/globalSlice";
 import productsReducer from "../features/products/productsSlice";
 import userReducer from "../features/authentication/userSlice";
 import cartReducer from "../features/cart/cartSlice";
+import addressReducer from "../features/authentication/addressSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     products: productsReducer,
     user: userReducer,
     cart: cartReducer,
+    address: addressReducer,
   },
 });
