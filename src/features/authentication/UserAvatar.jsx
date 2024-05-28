83261f56e835e61872d768965b5e9cefc95d6f3f
3nonch <enonch@hotmail.com>
2024-05-28 16:44:34 +0300

    Link added to user avatar

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,6 +1,7 @@
 import md5 from "md5";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser, selectUser } from "./userSlice";
+import { Link } from "react-router-dom";
 
 const UserAvatar = () => {
   const dispatch = useDispatch();
@@ -12,11 +13,14 @@ const UserAvatar = () => {
   };
   return (
     <div className="flex items-center gap-[0.5rem]">
-      <img
-        className="w-[3rem] rounded-[50%] cursor-pointer"
-        src={getUserAvatarURL("ciftcinanc@gmail.com")}
-        //   src={getUserAvatarURL(userInfo?.email)}
-      />
+      <Link to={"/account"}>
+        <img
+          className="w-[3rem] rounded-[50%] cursor-pointer"
+          src={getUserAvatarURL("ciftcinanc@gmail.com")}
+          //   src={getUserAvatarURL(userInfo?.email)}
+        />
+      </Link>
+
       <p className="text-[1.6rem] max-md:hidden">{userInfo?.name}</p>
       <button className="max-md:hidden" onClick={() => dispatch(logoutUser())}>
         X
