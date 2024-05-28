d671f062885eea1e3ea444e173c526ac421f5db9
3nonch <enonch@hotmail.com>
2024-05-28 21:48:00 +0300

    Credit Card component initial setup

--- src/ui/PaymentCard.jsx
+++ src/ui/PaymentCard.jsx
@@ -9,11 +9,23 @@ const PaymentCard = ({ card, setSelectedCard }) => {
     );
   };
 
+  console.log(card);
+
   const organizedNo = splitCardNumber(card?.card_no);
   return (
-    <div className="w-[49%] max-md:w-[100%] min-h-[150px] border-[1px] rounded-[3px] bg-slate-100 p-[1rem] flex flex-col justify-between">
-      <div className="flex justify-between items-center text-[3rem] text-blue-500">
-        <i className="fa-solid fa-building-columns"></i>
+    <li className="w-[32%] max-md:w-[100%] min-h-[150px] border-[1px] rounded-[3px] bg-slate-100 p-[1rem] flex flex-col justify-between relative">
+      <div className="flex items-center gap-[0.5rem] absolute top-[1rem] left-[1rem]">
+        <input
+          onChange={() => setSelectedCard(card)}
+          type="radio"
+          name="addressChoice"
+          id={card?.name}
+        />
+        <label className={`text-[1.4rem] font-[500] `} htmlFor={card?.name}>
+          {card.name_on_card}
+        </label>
+      </div>
+      <div className="flex flex-row-reverse items-center text-[3rem] text-blue-500">
         <i className="fa-brands fa-cc-mastercard"></i>
       </div>
       <div className="flex flex-col text-right">
@@ -22,7 +34,7 @@ const PaymentCard = ({ card, setSelectedCard }) => {
           {card.expire_month}/{card.expire_year}
         </span>
       </div>
-    </div>
+    </li>
   );
 };
 
